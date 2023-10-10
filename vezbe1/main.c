02e84a4df69de8570a32fd3a1353ffbe5f2d2b70
dalilahonic <dalilahonic1@gmail.com>
2023-10-10 14:29:26 +0200

    .

--- vezbe1/main.c
+++ vezbe1/main.c
@@ -12,9 +12,22 @@ int main() {
     int c = a + b;
 
     printf("%.2f \n", br);
+    printf("%s \n", ime);
 
     for(int i = 0; i < 10; i++) {
         printf("%d \n", i);
     }
+
+    float y;
+    y = (float)10 / 4;
+    printf("%f \n", y);
+
+    if(b > 10) {
+        printf("b je vece od 10");
+    }
+    else {
+        printf("b nije vece od 10");
+    };
+
     return 0;
 }

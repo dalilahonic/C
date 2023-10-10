75886be3b23dd1fadd7a301c91c8e0bf06534514
dalilahonic <dalilahonic1@gmail.com>
2023-10-10 13:55:34 +0200

    .

---
+++ vezbe1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h > 
+#include <stdlib.h >
+
+int main() {
+
+    float br = 1.2;
+    int a = 23;
+    char ime[] = "Dalila";
+
+    const int b = 16;
+
+    int c = a + b;
+
+    printf("%.2f \n", br);
+
+    for(int i = 0; i < 10; i++) {
+        printf("%d \n", i);
+    }
+    return 0;
+}

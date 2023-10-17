48b74d9384b2c7d81d6fefe90a5ff89ecbbe9a17
dalilahonic <dalilahonic1@gmail.com>
2023-10-17 14:27:16 +0200

    .

---
+++ vezbe2/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h > 
+#include <stdlib.h >
+
+int main() {
+
+    // zbir cifara trocifrenog broja.......................................
+
+    // int ABC, A, B,C, ZBIR;
+
+    // printf("Unesite trocifren broj");
+    // scanf("%d", &ABC);
+    // A = (ABC / 100);
+    // B = (ABC / 10) % 10;
+    // C = ABC % 10;
+    // ZBIR = A + B + C;
+    // printf("Zbir cifara broja %d je %d", ABC, ZBIR);
+
+    // int br1, br2, br3;
+    // printf("unesite prvi broj");
+    // scanf("%d", &br1);
+    // printf("unesite drugi broj");
+    // scanf("%d", &br2);
+    // br3 = br1 + br2;
+    // printf("Rezultat sabiranja ova dva broja je %d ", br3);
+
+    // program koji ucitava vrednost proizvoda u dinarima a zatim izracunava i pokazuje koliko je potrebno novcanica od 500 100 i 1 din za placanje tog proizvoda.....................................
+
+    // int cena;
+    // printf("Unesite vrednost");
+    // scanf("%d", &cena);
+    // int p = (cena / 500);
+    // int s = (cena % 500) / 100;
+    // int j = cena % 100;
+    // printf("treba imati %d novcanina od 500, %d novcaninca od 100 i %d novcaninca od 1", p, s, j);
+
+    //uneti sekunde i da dobijemo minute casove dana...................................
+
+    // int is, sek, m, sat, dan;
+    // printf("Unesite interval u sekundama");
+    // scanf("%d", &is);
+    // sek = is % 60;
+    // m = (is / 60) % 60;
+    // sat = ((is /60) /60) % 24;
+    // dan = ((is /60) /60) / 24;
+    // printf("%d sekundi %d minuta %d sat %d dana", sek, m, sat, dan);
+
+    // proveriti da li je broj paran ili neparan........................................
+
+    // int a;
+    // printf("unesite broj");
+    // scanf("%d", &a);
+    // if(a % 2 == 0) {
+    //     printf("%d je paran broj", a);
+    // } else {
+    //     printf("%d je neparan broj", a);
+    // }
+
+    // program koji ce ispitati da li je uneti broj pozitivan negativan ili jednak nul
+
+    // int b;
+    // printf("unesite broj");
+    // scanf("%d", &b);
+    // if(b > 0) printf("uneti broj je pozitivan");
+    // else if (b < 0) printf("uneti broj je negativan");
+    // else printf("uneti broj je jednak nuli");
+
+    // uneti a i b brojeve i da vidimo koji je broj veci........................
+
+    // int a, b;
+    // printf("uneti dva broja");
+    // scanf("%d %d", &a, &b);
+    // if(a != b) {
+    //     if(a > b) printf("prvi broj je veci od drugog");
+    //     else  printf("drugi broj je veci od prvog");
+    // } else {
+    //     printf("isti brojevi");
+    // }
+
+    // uneti 3 broja i videti koji je najveci
+
+    // int a, b, c, max;
+    // printf("uneti 3 broja");
+    // scanf("%d %d %d", &a, &b, &c);
+    // max = a;
+    // if(b > max) max= b;
+    // if(c >max) max = c;
+
+    // printf("nejveci broj je %d", max);
+
+
+    return 0;
+
+}

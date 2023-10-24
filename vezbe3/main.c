27a96b9b5e84daee059a815e4d6df696a2c62e2a
dalilahonic <dalilahonic1@gmail.com>
2023-10-24 15:28:27 +0200

    .

---
+++ vezbe3/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h > 
+#include <stdlib.h >
+
+int main() {
+    // int abc, a,b,c;
+    // printf("unesite trocifren broj");
+    // scanf("%d", &abc);
+    // a = (abc / 100);
+    // b = (abc / 10) % 10;
+    // c = abc % 10;
+
+
+    // if(a + b > c) printf("%d", a +b);
+    // else printf("nista");
+
+    //.........................
+
+    // int x, y;
+    // printf("Unesite zeljeni broj");
+    // scanf("%d", &x);
+    // if(x > -2 && x >= 2) {
+    //     printf("y = %d", 2*x);
+    // } else {
+    //     printf("y = %d", 5 * x);
+    // }
+
+    //..............................
+
+    // napisati prograt koji za uneti znak operacije + - * / ce izvrsiti sledecu operaciju
+
+    // int a, b;
+    // char operator;
+    // printf("Unesite dva broj ");
+    // scanf("%d %d", &a, &b);
+    // printf("Unesite operaciju");
+    // scanf(" %c", &operator);
+
+    //  switch (operator) {
+    //  case '+': 
+    //  printf("%d", a +b);
+    //     break;
+    //      case '-': 
+    //  printf("%d", a - b);
+    //     break;
+    //      case '*': 
+    //  printf("%d", a * b);
+    //     break;
+    //      case '/': 
+    //  printf("%d", a / b);
+    //     break;
+    //  }
+
+    //...........................................
+
+    // int broj, suma = 0;
+    // printf("Unesite broj");
+    // scanf("%d", &broj);
+    // for(int i = 1; i <= broj; i++) {
+    //     suma = suma + i;
+    // };
+
+    // printf("%d", suma);
+
+    //........................................
+
+    // int i, n, m, s = 1;
+    // printf("unesite dva broja");
+    // scanf("%d %d", &n, &m);
+    // for(i = 0; i <= m; i++) {
+    //     s = s * (n + i * m);
+    // }
+
+    // printf("suma je %d", s);
+
+
+    return 0;
+
+}
